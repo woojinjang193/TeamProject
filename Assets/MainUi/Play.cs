bf19f901505258aadd9bd8ca81e4fd542725de10
JungDaWit <ekdnlt10@gmail.com>
2025-04-28 12:19:55 +0900

    [Feat] 타이틀 버튼 추가
    타이틀과 버튼 추가

---
+++ Assets/MainUi/Play.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    private void Start()
+    {
+        GamePlay();
+    }
+    public void GamePlay()
+    {
+        Debug.Log("게임시작");
+        //SceneManager.LoadScene("scened")
+    }
+}
