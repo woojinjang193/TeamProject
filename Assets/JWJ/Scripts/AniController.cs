a0dcc868c645b5ad83f5d59f256f8cf97867095d
woojinjang193 <ryanjang193@gmail.com>
2025-04-30 02:41:14 +0900

    [Fix & Feat] 카메라 시점변경, 상하좌우, 넉백 애니메이션 추가
    카메라 시점변경, 상하좌우, 넉백 애니메이션 추가
    
    #bug : 카메라 시점 변경후 플레이어 물리적 충돌시 회전하는 버그

--- Assets/JWJ/Scripts/AniController.cs
+++ Assets/JWJ/Scripts/AniController.cs
@@ -6,45 +6,53 @@ public class AniController : MonoBehaviour
 {
 
     public Animator anima;
+    private PlayerController playerController;
+    private bool wasKnockback = false;
 
     // Start is called before the first frame update
     void Start()
     {
         anima = GetComponent<Animator>();
+        playerController = GetComponentInParent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             anima.SetTrigger("Attack");
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        bool isForward = Input.GetKey(KeyCode.W);
+        anima.SetBool("isForward", isForward);
+
+        bool isBack = Input.GetKey(KeyCode.S);
+        anima.SetBool("isBack", isBack);
+
+        bool isLeft = Input.GetKey(KeyCode.A);
+        anima.SetBool("isLeft", isLeft);
+
+        bool isRight = Input.GetKey(KeyCode.D);
+        anima.SetBool("isRight", isRight);
+
+        if (playerController.isKnockback && !wasKnockback)
         {
-            anima.SetTrigger("Test1");
+            anima.SetBool("isKnockback", true);
+            wasKnockback = true;
+            Debug.Log("플레이어 넉백모션");
+        }
+        else if (!playerController.isKnockback && wasKnockback)
+        {
+            anima.SetBool("isKnockback", false);
+            wasKnockback = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            anima.SetTrigger("Test2");
-        }
 
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            anima.SetTrigger("Test3");
-        }
+        //  bool iForward= Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
+        //  anima.SetBool("isMoving", iForward);
 
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            anima.SetTrigger("Test4");
-        }
 
-        bool isMoving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
-        anima.SetBool("isMoving", isMoving);
 
-        
-        
     }
 }
