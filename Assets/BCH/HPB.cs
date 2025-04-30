585ebfeade453961cb611e38ac7fe8f6086da44a
qnfdns120 <qorckdgus120@naver.com>
2025-04-30 11:46:46 +0900

    [fix] 케릭터 체력 ui 미기입 업데이트
    케릭터 체력 ui 미기입 업데이트

--- Assets/BCH/HPB.cs
+++ Assets/BCH/HPB.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HPB : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Slider hpbar;
+    private float maxHp = 100;
+    private float curHp = 100;
+
+    void Start()
+    {
+        hpbar.value = (float) curHp / (float) maxHp;
+    }
+    private void HandleHp()
+    {
+        hpbar.value = Mathf.Lerp(hpbar.value, (float)curHp / (float)maxHp, Time.deltaTime * 10);
+    }
+
 }
