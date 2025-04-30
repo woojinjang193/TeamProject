d97cba40932e87c6430ef55bf2adfbe18d65f6a7
정재영 <wodud04277@gmail.com>
2025-04-30 13:02:44 +0900

    [Feat] 몬스터 스포너 설정
    스폰주기와 스폰할 몬스터 수 유니티 에디터에서 수정 가능
    한 웨이브 몬스터를 전부 죽이면, 다음 웨이브 시작

--- Assets/JJY/Spawner/Spawner.cs
+++ Assets/JJY/Spawner/Spawner.cs
@@ -1,18 +1,64 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     public Wave[] waves;
+    public MonsterController monster;
 
-    //public Monster monster;
+    Wave curWave;                   //현재 웨이브
+    int curWaveNum;                 //현재 웨이브의 숫자
+
+    int spawnReadyMonster;          //스폰 해야할 남은 몬스터 수
+    int aliveMonsterCount;          //살아있는 몬스터 수
+
+    float nextSpawnTime;            //다음 스폰 시간
+
+    private void Start()
+    {
+        NextWave();
+    }
+
+    private void Update()
+    {
+        if (spawnReadyMonster > 0 && Time.time > nextSpawnTime)
+        {
+            spawnReadyMonster--;
+            nextSpawnTime = Time.time + curWave.timeBetweenSpawn;
+
+            MonsterController spawnMonster = Instantiate(monster, Vector3.zero, Quaternion.identity) as MonsterController;
+            spawnMonster.OnDeath += OnMonsterDeath;
+        }
+    }
+
+    void OnMonsterDeath()
+    {
+        aliveMonsterCount--;
+        if (aliveMonsterCount == 0)
+        {
+            NextWave();
+        }
+    }
+    void NextWave()
+    {
+        curWaveNum++;
+        if (curWaveNum - 1 < waves.Length)
+        {
+            Debug.Log("Wave : " + curWaveNum);
+            curWave = waves[curWaveNum - 1];
+            spawnReadyMonster = curWave.monsterCount;
+            aliveMonsterCount = spawnReadyMonster;
+        }
+        else
+        {
+            Debug.Log("Wave 끝");
+        }
+    }
 
     [Serializable]
     public class Wave
     {
-        public int monsterCount;
+        public int monsterCount;       //스폰할 몬스터의 합
         public float timeBetweenSpawn; //난이도에 따른 스폰주기
     }
 }
