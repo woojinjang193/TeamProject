853609d87d87d79a3c037eb74302dcec3f555e71
정재영 <wodud04277@gmail.com>
2025-05-02 13:04:05 +0900

    [Feat] 몬스터 공격, 피격, 사망시 애니메이션 추가
    죽었을 때 길을 막아서 죽으면 Istrigger = true;로 전환
    move 모션 수정 필요

--- Assets/JJY/Monster/MonsterAniController.cs
+++ Assets/JJY/Monster/MonsterAniController.cs
@@ -1,8 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterAniController : MonoBehaviour
+public class SpiderAniController : MonoBehaviour
 {
     public Animator anima;
     private MonsterController monsterController;
@@ -10,6 +9,9 @@ public class MonsterAniController : MonoBehaviour
     private float attackRange = 1.3f;
     private Transform player;
     private bool isAttacking;
+    private bool isDead = false;
+
+    [SerializeField] float deathdelay = 1.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,33 +25,41 @@ public class MonsterAniController : MonoBehaviour
 
     private void Update()
     {
-        if(monsterController.monsterHP < lastHP)
+        if(monsterController.monsterHP <= 0)  //몬스터 죽는모션
+        {
+            isDead = true;
+            anima.SetTrigger("MonsterDied");
+            StartCoroutine(DeathDelay());
+        }
+
+        if (monsterController.monsterHP < lastHP)
         {
             anima.SetTrigger("MonsterGetDamaged");
             lastHP = monsterController.monsterHP;
         }
 
-    float distance = Vector3.Distance(transform.position, player.position);  //가까이에 오면 공격
+        float distance = Vector3.Distance(transform.position, player.position);  //가까이에 오면 공격
 
         if (distance < attackRange && isAttacking == false)
         {
             anima.SetTrigger("MonsterAttack");
             isAttacking = true;
-            Invoke(nameof(AttackDelay), 0.5f); // 공격 딜레이
+            Invoke(nameof(AttackDelay), 1f); // 공격 딜레이
 
         }
 
-        }
+    }
 
     private void AttackDelay()  // 공격 딜레이
     {
         isAttacking = false;
     }
 
-        //  if(monsterController.monsterHP <= 0)  //몬스터 죽는모션
-        //  {
-        //      anima.SetTrigger("MonsterDied");
-        //      //lastHP = 0;
-        //  }
+    private IEnumerator DeathDelay()
+    {
+        yield return new WaitForSeconds(deathdelay);
+        Destroy(monsterController.gameObject);
     }
+
+}
 
